f13ff9c15133945d2818d54e323c8f7f3d0f6bb5
Alicia Saravia <alicia.saraviam@gmail.com>
2022-09-24 12:23:28 -0300

    Tarea Tabla Html

---
+++ server.py
@@ -0,0 +1,6 @@
+from tabla_html import app
+from tabla_html.controlador import controlador
+
+
+if __name__=="__main__":   # Asegúrate de que este archivo se esté ejecutando directamente y no desde un módulo diferente    
+    app.run(debug=True)    # Ejecuta la aplicación en modo de depuración
